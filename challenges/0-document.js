c2ff5ed539a7ed534768ebbed689f0ea27f4fa46
2405-002-ABD <2405-002-abd@U-ZTCCGG14L2B8.corp.amazonworkspaces.com>
2024-06-04 13:53:01 +0100

    complete asNegative with test cases

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,5 +1,14 @@
 // TODO (Ex.0): document and write tests for this function
 
+//Test cases
+console.log(asNegative(10));
+console.log(asNegative(5));
+console.log(asNegative(3));
+console.log(asNegative(100));
+console.log(asNegative(-45));
+console.log(asNegative(0));
+console.log(asNegative(-300));
+
 
 /**
  * Takes a number and returns a negative version of the number
@@ -7,8 +16,10 @@
  * @returns {number} the negative version of input number
  */
 function asNegative(n) {
-  let negativeResult = -n;
-  return negativeResult;
+  if (n <= 0) {
+    return n;
+  }
+  return -n
 }
 
-export { asNegative };
+// export { asNegative };
