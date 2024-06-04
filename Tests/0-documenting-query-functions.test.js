564ef447705ebbe69781c49c8d6e5dad6b033cf7
2405-002-ABD <2405-002-abd@U-ZTCCGG14L2B8.corp.amazonworkspaces.com>
2024-06-04 13:43:35 +0100

    write first test case

--- Tests/0-documenting-query-functions.test.js
+++ Tests/0-documenting-query-functions.test.js
@@ -1,1 +1,7 @@
 import { asNegative } from "../challenges/0-document";
+
+test("Makes positive number negative", () => {
+    const testCase = asNegative(10);
+    const expectedResult = -10;
+    expected(testCase).toEqual(expectedResult);
+})
